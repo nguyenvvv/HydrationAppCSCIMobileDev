3ca5e8bd67dcbedf8cfbe45290b8351d59da4a97
Vi Nguyen <nguyenvv@hendrix.edu>
2020-09-15 16:23:43 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 //import 'package:hexcolor/hexcolor.dart';
 
 void main() {
